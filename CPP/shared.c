c8c7fc0193295ce463cacc6fa859cdb39e5606af
guolanting <guolanting@haplox.com>
2015-12-20 07:29:38 +0000

    test shared library  and julia ccall

---
+++ CPP/shared.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void hello(){
+    printf("hello world\n");
+}
+
+
+int main(){
+
+
+    printf("In the main func\n");
+
+    return 0;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# hackerrank
-code while play on hackerrank 
